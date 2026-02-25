324e725ce2a45c299e4dc076441082940c3697f1
vohai04 <haivo3225@gmail.com>
2026-02-25 12:43:06 +0700

    Add Game Over Scene and Play Again Button- Back To Menu Button

---
+++ Assets/Project/Scripts/GameOverManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    // Gắn vào Button Play Again
+    public void OnPlayAgain()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    // Gắn vào Button Back to Menu
+    public void OnBackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}

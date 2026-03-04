72d86ba442d62e63860426755fe0824c3fce2d46
ptvuong <vuongphanphanvuong@gmail.com>
2026-03-04 10:39:37 +0700

    Create Setting and Select,Unlock Player

---
+++ Assets/Project/Scripts/Editor/GoldCheatEditor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Project.Scripts.Editor
+{
+    public class GoldCheatEditor : EditorWindow
+    {
+        private int goldAmount = 1000;
+
+        [MenuItem("Tools/Gold Cheat")]
+        public static void ShowWindow()
+        {
+            GetWindow<GoldCheatEditor>("Gold Cheat");
+        }
+
+        void OnGUI()
+        {
+            GUILayout.Label("Thêm vàng để test", EditorStyles.boldLabel);
+
+            goldAmount = EditorGUILayout.IntField("Số vàng:", goldAmount);
+
+            if (GUILayout.Button("Thêm vàng"))
+            {
+                AddGold(goldAmount);
+            }
+
+            if (GUILayout.Button("Reset Save Data"))
+            {
+                ResetSave();
+            }
+
+            GUILayout.Space(10);
+            GUILayout.Label($"Save path: {Application.persistentDataPath}/save.json", EditorStyles.miniLabel);
+        }
+
+        void AddGold(int amount)
+        {
+            SaveData data = SaveSystem.Load();
+            data.gold += amount;
+            SaveSystem.Save(data);
+            Debug.Log($"[GoldCheat] Đã thêm {amount} vàng. Tổng: {data.gold}");
+        }
+
+        void ResetSave()
+        {
+            SaveData data = new SaveData();
+            SaveSystem.Save(data);
+            Debug.Log("[GoldCheat] Đã reset save data!");
+        }
+    }
+}
---
+++ Assets/Project/Scripts/PlayerSelectManager.cs
@@ -0,0 +1,94 @@
+﻿using Assets.Project.Scripts;
+using System;
+using UnityEngine;
+
+/// <summary>
+/// PlayerSelectManager – Singleton qu?n lý vi?c m? khóa và ch?n Player.
+/// G?n vào GameObject trong scene Menu/PlayerSelect.
+/// Yêu c?u gán PlayerRegistry.
+/// </summary>
+public class PlayerSelectManager : MonoBehaviour
+{
+    public static PlayerSelectManager Instance;
+
+    [Header("Data")]
+    public PlayerRegistry playerRegistry;
+
+    private SaveData saveData;
+
+    public PlayerData SelectedPlayer { get; private set; }
+
+    public event Action OnSaveChanged;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        saveData = SaveSystem.Load();
+
+        // Mở khóa mặc định nhung player có unlockedByDefault
+        bool dirty = false;
+        foreach (var p in playerRegistry.allPlayers)
+        {
+            if (p.unlockedByDefault && !saveData.unlockedPlayers.Contains(p.playerID))
+            {
+                saveData.unlockedPlayers.Add(p.playerID);
+                dirty = true;
+            }
+        }
+        if (dirty) SaveSystem.Save(saveData);
+
+        // Khôi ph?c player ?ã ch?n
+        if (!string.IsNullOrEmpty(saveData.selectedPlayerId))
+            SelectedPlayer = playerRegistry.GetByID(saveData.selectedPlayerId);
+
+        // Fallback: ch?n player ??u tiên ?ã m? khóa
+        if (SelectedPlayer == null && saveData.unlockedPlayers.Count > 0)
+            SelectedPlayer = playerRegistry.GetByID(saveData.unlockedPlayers[0]);
+    }
+
+    public int GetGold() => saveData.gold;
+
+    public bool IsUnlocked(PlayerData data)
+        => saveData.unlockedPlayers.Contains(data.playerID);
+
+    public bool IsSelected(PlayerData data)
+        => SelectedPlayer != null && SelectedPlayer.playerID == data.playerID;
+
+    /// <summary>Tr? v? true n?u m? khóa thành công.</summary>
+    public bool TryUnlock(PlayerData data)
+    {
+        if (IsUnlocked(data)) return true;
+        if (saveData.gold < data.unlockCost) return false;
+
+        saveData.gold -= data.unlockCost;
+        saveData.unlockedPlayers.Add(data.playerID);
+        SaveSystem.Save(saveData);
+        OnSaveChanged?.Invoke();
+        return true;
+    }
+
+    public void SelectPlayer(PlayerData data)
+    {
+        if (!IsUnlocked(data)) return;
+
+        SelectedPlayer = data;
+        saveData.selectedPlayerId = data.playerID;
+        SaveSystem.Save(saveData);
+        OnSaveChanged?.Invoke();
+    }
+
+    /// <summary>Thêm vàng (g?i t? GameManager sau m?i màn).</summary>
+    public void AddGold(int amount)
+    {
+        saveData.gold += amount;
+        SaveSystem.Save(saveData);
+        OnSaveChanged?.Invoke();
+    }
+}
